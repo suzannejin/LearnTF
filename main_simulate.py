ed041b0bfc6bd4853a4c5433bbb45f8c2d7e43ca
alessiovignoli <alessio.vignoli3@studio.unibo.it>
2023-02-06 19:08:37 +0100

    added preliminary argparse section

--- main_simulate.py
+++ main_simulate.py
@@ -7,9 +7,13 @@ from src.datasetSimulation.TFFamilyClass import TfFamily
 
 def get_args():
     
-    parser = argparse.ArgumentParser(description="Generate a simulation dataset")
-    parser.add_argument("--ppm", type=str, help='path to the ppm file')
-    parser.add_argument('--prot', type=str, help="path to the prot file")
+    "get the arguments when using from the commandline"
+
+    parser = argparse.ArgumentParser(description="This script simulates data from a starting set of sequences and position-probabilities-matrices (ppm). This script is thought to work at Transcription Factor (TF) family level, for ex. Homeodomain. In praactical terms it is a sandbox for model testing, to achieve this:\n first it reads and parse two files (one for sequences one for ppm) matching the TF_id found in them; second it generates DNA sequences based on the ppm extracted.")
+    parser.add_argument("-p", "--ppm", type=str, required=True, metavar="FILE", help='path to the ppm file, the structure format of this file is better defined in the RADME.md of src/datasetSimulation/')
+    parser.add_argument("-s", '--prot', type=str, required=True, metavar="FILE", help="path to the prot file, as above ")
+    # optional flag example
+    # parser.add_argument("-o", "--opt", type=int, required=False,)
 
     args = parser.parse_args()
     return args   
