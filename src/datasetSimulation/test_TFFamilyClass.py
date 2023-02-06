b6423ba8e8c5be9a841f473e077723422a5881e5
mgrapotte <mathysgrapotte@gmail.com>
2023-02-06 17:38:13 +0100

    adding TF_family class tests and data + finishing parser main code TODO:doc

---
+++ src/datasetSimulation/test_TFFamilyClass.py
@@ -0,0 +1,16 @@
+import unittest
+import os
+from TFFamilyClass import TfFamily
+
+class TfFamilyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.TfFamily_object = TfFamily(pwm_file="test_TFFamilyData/test_PWM.txt", prot_file="test_TFFamilyData/test_prot_seq.txt")
+
+    def test_parse_prot_shape(self) -> None:
+        TF_prot_ID, prot = self.TfFamily_object._parseProt(self.TfFamily_object.prot_file)
+        self.assertEqual(len(TF_prot_ID), len(prot), "ID // protein mismatch")
+
+    def test_parse_prot(self) -> None:
+        TF_prot_ID, prot = self.TfFamily_object._parseProt(self.TfFamily_object.prot_file)
+        self.assertEqual('MQKATYYDSSAIYGGYPYQAANGFAYNASQQPYAPSAALGTDGVEYHRPACSLQSPASAGGHPKTHELSEACLRTLSGPPSQPPGLGEPPLPPPPPQAAPPAPQPPQPPPQPPAPTPAAPPPPSSVSPPQSANSNPTPASTAKSPLLNSPTVGKQIFPWMKESRQNTKQKTSGSSSGESCAGDKSPPGQASSKRARTAYTSAQLVELEKEFHFNRYLCRPRRVEMANLLNLTERQIKIWFQNRRMKYKKDQKGKGMLTSSGGQSPSRSPVPPGAGGYLNSMHSLVNSVPYEPQSPPPFSKPPQGAYGLPPASYPAPLPSCAPPPPPQKRYTAAGSGAGGTPDYDPHAHGLQGNGSYGTPHLQGSPVFVGGSYVEPMSNSGPLFGLTHLPHTTSAAMDYGGTGPLGSGHHHGPGPGEPHPTYTDLTAHHPSQGRIQEAPKLTHL', prot[4], "incorrect protein match")
