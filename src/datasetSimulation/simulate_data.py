99bbfb1c0d2bbe6f8b1a5f200536457b6d6997e7
suzannejin <suzanne.jin.yao@gmail.com>
2023-02-06 17:32:12 +0100

    update simulate_data.py script

--- src/datasetSimulation/simulate_data.py
+++ src/datasetSimulation/simulate_data.py
@@ -1,13 +1,14 @@
+import numpy as np
+import random
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-import numpy as np
 
 class SimulatedData: 
     
     def __init__(self, TfFamily):
         self.TfFamily = TfFamily
 
-    def compute_scores(, l=100, n=1000):
+    def simulate_data(self, l=100, n=1000):
         """ Simulate DNA sequences and score them to each PWM for the entire TF family.
     
         Params
@@ -21,39 +22,42 @@ class SimulatedData:
         """
         
         # generate list of dna sequences
-        self.dna_seqs = generate_list_dna_seq()
+        self.dna_seqs = self._generate_list_dna_seq_enriched_with_ppm()
 
         # create empty numpy array 
         scores = np.empty((len(tfs), len(self.dna_seqs)), dtype = float)
         
-        # calculate score for each TF PWM and DNA sequence
+        # calculate score for each TF PPM and DNA sequence
         for i,tfid in enumerate(tfs):
-            pwm = self.TfFamily.tfid2pwm(tfid)
+            ppm = self.TfFamily.tfid2ppm(tfid)
             for j,seq in enumerate(self.dna_seqs):
-                scores[i,j] = score_dna_seq_with_pwm(seq, pwm)
+                scores[i,j] = score_dna_seq_with_ppm(seq, ppm)
                 
         self.scores = scores
 
     @staticmethod
-    def _generate_list_dna_seq(l=100, n=1000):
-        """ Generate a list of n random DNA sequences, with length l.
+    def _generate_list_dna_seq_enriched_with_ppm(ppm, l=100, n=1000):
+        """ Generate a list of n random DNA sequences, with length l, and enriched by a given PWM.
         
         Params
         ------
+        ppm     (numpy float array) : Position probability matrix. rows = ACGT, cols = positions
         l       (int)   : The wanted length of the DNA sequence
         n       (int)   : Number of DNA sequences to be generated
         
         Output
         ------
-        dna_seqs (list of str)   : List of random DNA sequences
+        dna_seqs (list of str)   : List of DNA sequences
         """
         dna_seqs = []
         for i in range(n):
-            dna_seqs.append( generate_dna_seq(l) )
+            dna_seq = self._generate_random_dna_seq(l)
+            dna_seq = self._enrich_dna_seq_with_ppm(dna_seq, ppm)
+            dna_seqs.append(dna_seq)
         return dna_seqs
 
     @staticmethod
-    def _generate_dna_seq(l=100):
+    def _generate_random_dna_seq(l=100):
         """ Generate a random DNA sequence, with length l.
         
         Params
@@ -64,11 +68,40 @@ class SimulatedData:
         ------
         dna_seq (str)   : Generated random DNA sequence
         """
-        import random
-
         dna_seq = ''.join(random.choice('CGTA') for _ in xrange(l))
-
         return dna_seq
+
+    @staticmethod
+    def _enrich_dna_seq_with_ppm(dna_seq, ppm):
+        """ Enrich the DNA sequence with PPM. The starting position of the enriched region is also randomly selected.
+
+        Params
+        ------
+        dna_seq (str)               : A random DNA sequence
+        ppm     (numpy float array) : Position probability matrix. rows = ACGT, cols = positions
+        
+        Output
+        ------
+        enriched_seq (str) : The DNA sequence enriched with PPM
+        """
+
+        # create DNA chunk matching the given PPM matrix
+        bps   = ['A', 'C', 'G', 'T']
+        npos  = ppm.shape[1]
+        chunk = []
+        for i in range(npos):
+            pos_distr  = ppm[:,i]
+            current_bp = random.choices(bps, pos_distr)
+            chunk.append(current_bp)
+
+        # modify DNA sequence
+        start = random.choice(range(len(dna_seq)-npos))
+        end   = start + npos
+        enriched_seq = list(dna_seq)
+        enriched_seq[start:end] = chunk
+        enriched_seq = ''.join(enriched_seq)
+
+        return enriched_seq
 
     @staticmethod
     def onehote(sequence):
@@ -95,7 +128,8 @@ class SimulatedData:
         
         return onehot_encoded_seq
 
-    def score_dna_seq_with_pwm(self, seq, pwm):
+    @staticmethod
+    def _score_dna_seq_with_pwm(seq, pwm):
         """
         Scores a DNA sequence given a PWM
 
