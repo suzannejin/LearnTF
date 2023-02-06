7edf139797d808b61b824b8e73644070ef6f875f
alessiovignoli <alessio.vignoli3@studio.unibo.it>
2023-02-06 19:01:28 +0100

    fixed the merge, by removing merge line conflict

--- main_simulate.py
+++ main_simulate.py
@@ -6,30 +6,19 @@ from src.datasetSimulation.simulate_data import SimulatedData
 from src.datasetSimulation.TFFamilyClass import TfFamily
 
 def get_args():
-<<<<<<< HEAD
-    "get the arguments when using from the commandline"
     
-    return args
-=======
     parser = argparse.ArgumentParser(description="Generate a simulation dataset")
     parser.add_argument("--ppm", type=str, help='path to the ppm file')
     parser.add_argument('--prot', type=str, help="path to the prot file")
 
     args = parser.parse_args()
     return args   
->>>>>>> main
 
 
 
 
 if __name__ == "__main__":
     args = get_args()
-<<<<<<< HEAD
-
-=======
     tf_data = TfFamily(args.ppm, args.prot)
     simulated_data = SimulatedData(tf_data)
     simulated_data.simulate_data()
-    
->>>>>>> main
-    
