3fd9f2c03e8ada732adb0d555299a40f56413ccd
luisas <luisa.santus95@gmail.com>
2023-02-06 18:54:38 +0100

    adding main_simulate.py again

---
+++ main_simulate.py
@@ -0,0 +1,25 @@
+"""
+main script for generating a simulated dataset
+"""
+import argparse
+from src.datasetSimulation.simulate_data import SimulatedData
+from src.datasetSimulation.TFFamilyClass import TfFamily
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Generate a simulation dataset")
+    parser.add_argument("--ppm", type=str, help='path to the ppm file')
+    parser.add_argument('--prot', type=str, help="path to the prot file")
+
+    args = parser.parse_args()
+    return args   
+
+
+
+
+if __name__ == "__main__":
+    args = get_args()
+    tf_data = TfFamily(args.ppm, args.prot)
+    simulated_data = SimulatedData(tf_data)
+    simulated_data.simulate_data()
+    
+    
