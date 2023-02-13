31d832bf874d037f50de421f6fe02fb8be2fe48d
luisas <luisa.santus95@gmail.com>
2023-02-13 12:54:53 +0100

    convolute on pwm

--- src/datasetSimulation/test_TFFamilyClass.py
+++ src/datasetSimulation/test_TFFamilyClass.py
@@ -5,7 +5,7 @@ from TFFamilyClass import TfFamily
 class TfFamilyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.TfFamily_object = TfFamily(pwm_file="test_TFFamilyData/test_PWM.txt", prot_file="test_TFFamilyData/test_prot_seq.txt")
+        self.TfFamily_object = TfFamily(ppm_file="test_TFFamilyData/test_PWM.txt", prot_file="test_TFFamilyData/test_prot_seq.txt")
 
     def test_parse_prot_shape(self) -> None:
         TF_prot_ID, prot = self.TfFamily_object._parseProt(self.TfFamily_object.prot_file)
@@ -14,3 +14,4 @@ class TfFamilyTestCase(unittest.TestCase):
     def test_parse_prot(self) -> None:
         _, prot = self.TfFamily_object._parseProt(self.TfFamily_object.prot_file)
         self.assertEqual('MQKATYYDSSAIYGGYPYQAANGFAYNASQQPYAPSAALGTDGVEYHRPACSLQSPASAGGHPKTHELSEACLRTLSGPPSQPPGLGEPPLPPPPPQAAPPAPQPPQPPPQPPAPTPAAPPPPSSVSPPQSANSNPTPASTAKSPLLNSPTVGKQIFPWMKESRQNTKQKTSGSSSGESCAGDKSPPGQASSKRARTAYTSAQLVELEKEFHFNRYLCRPRRVEMANLLNLTERQIKIWFQNRRMKYKKDQKGKGMLTSSGGQSPSRSPVPPGAGGYLNSMHSLVNSVPYEPQSPPPFSKPPQGAYGLPPASYPAPLPSCAPPPPPQKRYTAAGSGAGGTPDYDPHAHGLQGNGSYGTPHLQGSPVFVGGSYVEPMSNSGPLFGLTHLPHTTSAAMDYGGTGPLGSGHHHGPGPGEPHPTYTDLTAHHPSQGRIQEAPKLTHL', prot[4], "incorrect protein match")
+
