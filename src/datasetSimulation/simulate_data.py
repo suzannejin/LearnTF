355db1741966717442ac71e7422ca0abb759165e
mgrapotte <mathysgrapotte@gmail.com>
2023-02-13 15:39:08 +0100

    taking pwms into account

--- src/datasetSimulation/simulate_data.py
+++ src/datasetSimulation/simulate_data.py
@@ -46,14 +46,14 @@ class SimulatedData:
 
         # create empty numpy array
         self.dna_seqs = self.generate_list_dna_seq_for_all_ppms(l, n)
-        scores = np.zeros((len(self.TfFamily.get_ppms()), len(self.dna_seqs)), dtype = float)
+        scores = np.zeros((len(self.TfFamily.get_pwms()), len(self.dna_seqs)), dtype = float)
 
         # compute score for each PPM and DNA sequence
-        for i,ppm in enumerate(self.TfFamily.get_ppms()):
+        for i,pwm in enumerate(self.TfFamily.get_pwms()):
             for j,seq in enumerate(self.dna_seqs):
                 # seq = self._sequence_to_array(seq) Uncomment this line once sklearn encoder has been fixed.
                 one_hot = self._onehote(seq)
-                scores[i,j] = self._convolve(ppm, one_hot).max()
+                scores[i,j] = self._convolve(pwm, one_hot).max()
 
         self.scores = scores
 
@@ -160,16 +160,16 @@ class SimulatedData:
     # TODO: Protein encoder that takes as input a sequences array and returns an encoded protein <- actually, should we do it here ? or during model training? 
 
     @staticmethod
-    def _convolve(ppm, one_hot_seq):
+    def _convolve(pwm, one_hot_seq):
         """
         Computes the convolution operation between a one hot encoded sequence and a ppm
         """
-        W = ppm.shape[1] # Window size
+        W = pwm.shape[1] # Window size
         L = one_hot_seq.shape[1]-W+1 # Find out about output size
         out = np.zeros(L) # Create output
         for i in range(L): 
             # For each sliding window, compute sum(ppm * seqlet) // convolution
-            out[i] = np.multiply(ppm, one_hot_seq[:,i:i+W]).sum()
+            out[i] = np.multiply(pwm, one_hot_seq[:,i:i+W]).sum()
         return out
 
 
